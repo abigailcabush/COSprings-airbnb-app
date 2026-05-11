8e55e820150bb7be8c0b322480f7722b28b43264
abigailcabush <abigailcabush@gmail.com>
2026-05-11 06:12:24 +0000

    update success message

--- app.py
+++ app.py
@@ -88,10 +88,10 @@ if user_name != "Select a name...":
         total_p = int(p_night * n_count)
         
         st.success(
-            f" **Selection Saved!**\n\n"
-            f"{current_bed['room']}: {current_bed['description']}\n\n"
+            f"✅ **Selection Saved!**\n\n"
+            f"**{current_bed['room']}**: {current_bed['description']}\n\n"
             f"\n\n" 
-            f"**Total Cost**: approximately \${total_p} ± \$15"
+            f"**Total Cost**: approximately \${total_p} ± \$15 for {n_count} days"
         )
         
         if st.button("Clear My Selection"):
