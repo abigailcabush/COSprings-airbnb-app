87ef2f721984d397cadad0546fc9fb8c5f505670
abigailcabush <abigailcabush@gmail.com>
2026-05-11 06:12:57 +0000

    again: removed bold

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if user_name != "Select a name...":
         
         st.success(
             f"✅ **Selection Saved!**\n\n"
-            f"**{current_bed['room']}**: {current_bed['description']}\n\n"
+            f"{current_bed['room']}, {current_bed['description']}\n\n"
             f"\n\n" 
             f"**Total Cost**: approximately \${total_p} ± \$15 for {n_count} days"
         )
